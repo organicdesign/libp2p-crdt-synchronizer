9977e6f867d3a05c9b5603ecba4b90cde5304715
saul <saul@organicdesign.nz>
2022-11-08 13:49:18 +1300

    Send/receive data using the protocol.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import type { Libp2p } from "libp2p";
-import { DistributedStateSynchronizer } from "distributed-state-synchronizer";
+// import { DistributedStateSynchronizer } from "distributed-state-synchronizer";
+import { StreamWrapper } from "./StreamWrapper.js";
+import type { Connection, Stream } from "@libp2p/interface-connection";
+
 
 const PROTOCOL = "/libp2p-state-replication/0.0.1";
 
 export class StateReplicator {
 	private node: Libp2p;
+	private streams = new Map<string, StreamWrapper>();
 
 	constructor(node: Libp2p) {
-		node.handle(PROTOCOL, ({ connection, stream }) => {
-			console.log("handling");
+		node.handle(PROTOCOL, async ({ connection, stream }) => {
+			this.handleStream(connection, stream);
 		});
 
 		this.node = node;
+
+		setTimeout(() => {
+			setInterval(() => this.doSomething(), 5000);
+		}, 35000);
 	}
 
 	async establishProtocols () {
@@ -24,19 +32,36 @@ export class StateReplicator {
 
 			// This will throw if the node does not support this proto
 			const stream = await connection.newStream(PROTOCOL);
-
-			console.log("connected");
+			this.handleStream(connection, stream);
 		}
 	}
 
 	async doSomething () {
-		const streams = this.node.connectionManager.getConnections()
-			.map(c => c.streams)
-			.reduce((a, c) => [...a, ...c], [])
-			.filter(s => s.stat.protocol === PROTOCOL);
+		await this.establishProtocols();
 
-		for (const stream of streams) {
+		for (const stream of this.streams.values()) {
 			// Do something
+			stream.write(new Uint8Array([1, 2, 3]));
 		}
+	}
+
+	private async handleStream (connection: Connection, stream: Stream) {
+		const remotePeer = connection.remotePeer.toString();
+
+		const wrapper = new StreamWrapper(stream);
+
+		this.streams.set(remotePeer, wrapper);
+
+
+		try {
+			for await (const val of wrapper.reader) {
+				console.log("GOT DATA:", val);
+			}
+		} catch (error) {
+			console.warn(error);
+		}
+
+		wrapper.close();
+		this.streams.delete(remotePeer);
 	}
 }
