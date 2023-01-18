5bcc546fcbd7df37df5adcddcd34cd4c49133dba
saul <saul@organicdesign.co.nz>
2023-01-19 10:09:24 +1300

    Add tests for getting and setting CRDTs.

--- test/CRDTSynchronizer.spec.ts
+++ test/CRDTSynchronizer.spec.ts
@@ -1,12 +1,13 @@
 import type { PeerId } from "@libp2p/interface-peer-id";
 import type { ConnectionManager } from "@libp2p/interface-connection-manager";
+import type { CRDT } from "@organicdesign/crdt-interfaces";
 import { mockRegistrar, mockConnectionManager, mockNetwork } from "@libp2p/interface-mocks";
 import { stubInterface } from "ts-sinon";
 import { start } from "@libp2p/interfaces/startable";
 import { createRSAPeerId } from "@libp2p/peer-id-factory";
 import { CRDTSynchronizerComponents, CRDTSynchronizer, createCRDTSynchronizer } from "../src/CRDTSynchronizer";
 
-export const createComponents = async (): Promise<CRDTSynchronizerComponents & { peerId: PeerId }> => {
+const createComponents = async (): Promise<CRDTSynchronizerComponents & { peerId: PeerId }> => {
 	const components: CRDTSynchronizerComponents & { peerId: PeerId } = {
 		peerId: await createRSAPeerId({ bits: 512 }),
 		registrar: mockRegistrar(),
@@ -20,6 +21,18 @@ export const createComponents = async (): Promise<CRDTSynchronizerComponents & {
 	mockNetwork.addNode(components);
 
 	return components;
+};
+
+const mockCRDT = (): CRDT => {
+	return {
+		id: new Uint8Array(),
+
+		sync (): Uint8Array {
+			return new Uint8Array();
+		},
+
+		toValue () {}
+	};
 };
 
 let synchronizer: CRDTSynchronizer, components: CRDTSynchronizerComponents & { peerId: PeerId };
@@ -45,5 +58,33 @@ describe("startable interface", () => {
 		await synchronizer.stop();
 
 		expect(synchronizer.isStarted()).toBe(false);
+	});
+});
+
+describe("crdts", () => {
+	beforeEach(async () => {
+		await synchronizer.start();
+	});
+
+	it("sets and gets crdts", () => {
+		const keys = ["test-1", "test-2", "test-3"];
+
+		for (const key of keys) {
+			const crdt = mockCRDT();
+
+			synchronizer.setCRDT(key, crdt);
+
+			expect(synchronizer.getCRDT(key)).toStrictEqual(crdt);
+		}
+	});
+
+	it("lists crdt names", () => {
+		const keys = ["test-1", "test-2", "test-3"];
+
+		for (const key of keys) {
+			synchronizer.setCRDT(key, mockCRDT());
+		}
+
+		expect(synchronizer.CRDTNames).toStrictEqual(keys);
 	});
 });
