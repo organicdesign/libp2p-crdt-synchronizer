9dd53447f999750236e00e38176f1ce5cc65efc7
saul <saul@organicdesign.co.nz>
2023-01-25 13:52:58 +1300

    Add another sync protocol message type.

--- src/CRDTSyncProtocol.ts
+++ src/CRDTSyncProtocol.ts
@@ -123,3 +123,80 @@ export namespace SyncMessage {
     return decodeMessage(buf, SyncMessage.codec())
   }
 }
+
+export interface SyncMessageWrapper {
+  id: number
+  data: Uint8Array
+  request?: boolean
+}
+
+export namespace SyncMessageWrapper {
+  let _codec: Codec<SyncMessageWrapper>
+
+  export const codec = (): Codec<SyncMessageWrapper> => {
+    if (_codec == null) {
+      _codec = message<SyncMessageWrapper>((obj, w, opts = {}) => {
+        if (opts.lengthDelimited !== false) {
+          w.fork()
+        }
+
+        if (opts.writeDefaults === true || obj.id !== 0) {
+          w.uint32(8)
+          w.uint32(obj.id)
+        }
+
+        if (opts.writeDefaults === true || (obj.data != null && obj.data.byteLength > 0)) {
+          w.uint32(18)
+          w.bytes(obj.data)
+        }
+
+        if (obj.request != null) {
+          w.uint32(24)
+          w.bool(obj.request)
+        }
+
+        if (opts.lengthDelimited !== false) {
+          w.ldelim()
+        }
+      }, (reader, length) => {
+        const obj: any = {
+          id: 0,
+          data: new Uint8Array(0)
+        }
+
+        const end = length == null ? reader.len : reader.pos + length
+
+        while (reader.pos < end) {
+          const tag = reader.uint32()
+
+          switch (tag >>> 3) {
+            case 1:
+              obj.id = reader.uint32()
+              break
+            case 2:
+              obj.data = reader.bytes()
+              break
+            case 3:
+              obj.request = reader.bool()
+              break
+            default:
+              reader.skipType(tag & 7)
+              break
+          }
+        }
+
+        return obj
+      })
+    }
+
+    return _codec
+  }
+
+  export const encode = (obj: SyncMessageWrapper): Uint8Array => {
+    return encodeMessage(obj, SyncMessageWrapper.codec())
+  }
+
+  export const decode = (buf: Uint8Array | Uint8ArrayList): SyncMessageWrapper => {
+    return decodeMessage(buf, SyncMessageWrapper.codec())
+  }
+}
