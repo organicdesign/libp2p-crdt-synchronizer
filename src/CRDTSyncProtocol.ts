339c7a4d9f5a94caebcc5349c827b042dcb5ea5b
saul <saul@organicdesign.co.nz>
2023-01-30 08:58:08 +1300

    Start work on stateless crdt map synchronizer.

--- src/CRDTSyncProtocol.ts
+++ src/CRDTSyncProtocol.ts
@@ -124,6 +124,119 @@ export namespace SyncMessage {
   }
 }
 
+export enum StatelessMessageType {
+  SELECT = 'SELECT',
+  SELECT_RESPONSE = 'SELECT_RESPONSE',
+  SYNC = 'SYNC',
+  SYNC_RESPONSE = 'SYNC_RESPONSE'
+}
+
+enum __StatelessMessageTypeValues {
+  SELECT = 0,
+  SELECT_RESPONSE = 1,
+  SYNC = 2,
+  SYNC_RESPONSE = 3
+}
+
+export namespace StatelessMessageType {
+  export const codec = (): Codec<StatelessMessageType> => {
+    return enumeration<StatelessMessageType>(__StatelessMessageTypeValues)
+  }
+}
+export interface StatelessSyncMessage {
+  type: StatelessMessageType
+  sync?: Uint8Array
+  crdt?: string
+  protocol?: string
+  accept?: boolean
+}
+
+export namespace StatelessSyncMessage {
+  let _codec: Codec<StatelessSyncMessage>
+
+  export const codec = (): Codec<StatelessSyncMessage> => {
+    if (_codec == null) {
+      _codec = message<StatelessSyncMessage>((obj, w, opts = {}) => {
+        if (opts.lengthDelimited !== false) {
+          w.fork()
+        }
+
+        if (opts.writeDefaults === true || (obj.type != null && __StatelessMessageTypeValues[obj.type] !== 0)) {
+          w.uint32(8)
+          StatelessMessageType.codec().encode(obj.type, w)
+        }
+
+        if (obj.sync != null) {
+          w.uint32(18)
+          w.bytes(obj.sync)
+        }
+
+        if (obj.crdt != null) {
+          w.uint32(26)
+          w.string(obj.crdt)
+        }
+
+        if (obj.protocol != null) {
+          w.uint32(34)
+          w.string(obj.protocol)
+        }
+
+        if (obj.accept != null) {
+          w.uint32(40)
+          w.bool(obj.accept)
+        }
+
+        if (opts.lengthDelimited !== false) {
+          w.ldelim()
+        }
+      }, (reader, length) => {
+        const obj: any = {
+          type: StatelessMessageType.SELECT
+        }
+
+        const end = length == null ? reader.len : reader.pos + length
+
+        while (reader.pos < end) {
+          const tag = reader.uint32()
+
+          switch (tag >>> 3) {
+            case 1:
+              obj.type = StatelessMessageType.codec().decode(reader)
+              break
+            case 2:
+              obj.sync = reader.bytes()
+              break
+            case 3:
+              obj.crdt = reader.string()
+              break
+            case 4:
+              obj.protocol = reader.string()
+              break
+            case 5:
+              obj.accept = reader.bool()
+              break
+            default:
+              reader.skipType(tag & 7)
+              break
+          }
+        }
+
+        return obj
+      })
+    }
+
+    return _codec
+  }
+
+  export const encode = (obj: StatelessSyncMessage): Uint8Array => {
+    return encodeMessage(obj, StatelessSyncMessage.codec())
+  }
+
+  export const decode = (buf: Uint8Array | Uint8ArrayList): StatelessSyncMessage => {
+    return decodeMessage(buf, StatelessSyncMessage.codec())
+  }
+}
+
 export interface SyncMessageWrapper {
   id: number
   data: Uint8Array
---
+++ src/stateless-crdt-map-synchronizer.ts
@@ -0,0 +1,116 @@
+import type { CRDTSynchronizer, CRDT, SynchronizableCRDT, SyncContext } from "@organicdesign/crdt-interfaces";
+import { StatelessSyncMessage, StatelessMessageType } from "./CRDTSyncProtocol.js";
+
+export interface CRDTMapSyncComponents {
+	getCRDTKeys (): Iterable<string>
+	getCrdt (key: string): CRDT | SynchronizableCRDT | undefined
+	getId (): Uint8Array
+}
+
+export interface CRDTMapSyncOpts {
+	protocol: string
+}
+
+const isSynchronizableCRDT = (crdt: CRDT) => crdt["getSynchronizer"] && crdt["getSynchronizerProtocols"];
+
+export class CRDTMapSynchronizer implements CRDTSynchronizer {
+	public readonly protocol: string;
+	private readonly components: CRDTMapSyncComponents;
+
+	constructor(components: CRDTMapSyncComponents, options: Partial<CRDTMapSyncOpts> = {}) {
+		this.protocol = options.protocol ?? "/crdt-map/0.1.0";
+		this.components = components;
+	}
+
+	sync (data: Uint8Array | undefined, context: SyncContext): Uint8Array | undefined {
+		if (data == null) {
+			const crdt = this.getNextCRDT();
+
+			return StatelessSyncMessage.encode({
+				type: StatelessMessageType.SELECT,
+				crdt
+			});
+		}
+
+		const message = StatelessSyncMessage.decode(data);
+
+		switch (message.type) {
+			case StatelessMessageType.SELECT_RESPONSE:
+				if (message.accept) {
+					if (message.crdt != null && message.protocol != null) {
+						// Accepted both protocol and crdt
+					}
+
+					if (message.crdt != null) {
+						// Accepted only CRDT
+					}
+
+					throw new Error("SELECT_RESPONSE must include crdt");
+				} else {
+					if (message.crdt != null && message.protocol != null) {
+					}
+
+					if (message.crdt != null) {
+						// Rejected only CRDT
+						const crdt = this.getNextCRDT(message.crdt);
+
+						if (crdt == null) {
+							return new Uint8Array();
+						}
+
+						return StatelessSyncMessage.encode({
+							type: StatelessMessageType.SELECT,
+							crdt
+						});
+					}
+
+					throw new Error("SELECT_RESPONSE must include crdt");
+				}
+			case StatelessMessageType.SYNC_RESPONSE:
+				return this.handleCRDTSyncResponse(message, context.id);
+			case StatelessMessageType.SYNC:
+				return this.handleCRDTSync(message, context.id);
+			case StatelessMessageType.SELECT:
+				return this.handleCRDTSelect(message, context.id);
+			default:
+				throw new Error(`recieved unknown message type: ${message.type}`);
+		}
+	}
+
+	private getNextCRDT(last?: string) {
+		return this.getNext(this.components.getCRDTKeys(), last);
+	}
+
+	private getNextProtocol(crdtName: string, last?: string) {
+		const crdt = this.components.getCrdt(crdtName);
+
+		if (crdt == null || !isSynchronizableCRDT(crdt)) {
+			return;
+		}
+
+		const iterable = (crdt as SynchronizableCRDT).getSynchronizerProtocols();
+
+		return this.getNext(iterable, last);
+	}
+
+	private getNext (iterable: Iterable<string>, last?: string) {
+		const iterator = iterable[Symbol.iterator]();
+		let curr = iterator.next();
+
+		if (last == null) {
+			return curr.value;
+		}
+
+		while (!curr.done) {
+			if (curr.value === last) {
+				return iterator.next().value;
+			}
+
+			curr = iterator.next();
+		}
+
+		return curr.value;
+	}
+}
+
+export const createCRDTMapSynchronizer = (options?: Partial<CRDTMapSyncOpts>) => (components: CRDTMapSyncComponents) => new CRDTMapSynchronizer(components, options);
